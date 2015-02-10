4ccf812da551c38800aa2d0ff894f68f1d9abfa2
fangminglu <xiaofangwuyu@outlook.com>
2015-02-10 16:50:42 +0800

    加减

--- text_ce/addItems.js
+++ text_ce/addItems.js
@@ -45,3 +45,28 @@ function addNewItem(newItem)
     allItemsStr=JSON.stringify(allItems);
     localStorage.setItem('allItemsDb',allItemsStr)
 }
+$(function() {
+    $(".add").click(function () {
+        var t = $(this).parent().find('input[class*=text_box]');
+        t.val(parseInt(t.val()) + 1)
+        setTotal();
+    })
+    $(".min").click(function () {
+        var t = $(this).parent().find('input[class*=text_box]');
+        t.val(parseInt(t.val()) - 1)
+        if (parseInt(t.val()) < 0) {
+            t.val(0);
+        }
+        setTotal();
+    })
+    function setTotal() {
+        var s = 0;
+        $("#tab td").each(function () {
+            s += parseInt($(this).find('input[class*=text_box]').val())
+            * parseFloat($(this).find('span[class*=price]').text());
+        });
+        $("#total").html(s.toFixed(2));
+    }
+
+    setTotal();
+})
